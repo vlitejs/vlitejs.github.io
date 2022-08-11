b3e87aa2e59b351b50c62bd522e2d26aba0f40bb
Yoriiis <joris.daniel@gmail.com>
2022-08-12 00:15:37 +0200

    Update DM video id

--- src/demo/assets/scripts/templates/dailymotion-video.js
+++ src/demo/assets/scripts/templates/dailymotion-video.js
@@ -1,3 +1,3 @@
 export default function () {
-	return '<div id="player" class="vlite-js" data-video-id="x403kr8"></div><p class="content-copyright"><a href="https://www.youtube.com/watch?v=C4qgAaxB_pc" target="_blank" rel="noopener noreferrer">The Jungle Book &copy; Walt Disney Animation Studios</a></p>'
+	return '<div id="player" class="vlite-js" data-video-id="x88trc6"></div><p class="content-copyright"><a href="https://www.youtube.com/watch?v=C4qgAaxB_pc" target="_blank" rel="noopener noreferrer">The Jungle Book &copy; Walt Disney Animation Studios</a></p>'
 }
