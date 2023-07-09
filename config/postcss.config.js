3b998168ec7b20c9eb6e0647221b0e7a3dda8ea0
Yoriiis <joris.daniel@gmail.com>
2023-07-09 16:06:21 +0200

    Remove fn in postcss config

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,24 +1,22 @@
-module.exports = (api) => {
-	return {
-		plugins: [
-			'postcss-import',
-			'postcss-url',
-			'postcss-nested',
-			[
-				'postcss-preset-env',
-				{
-					stage: 2,
-					features: {
-						'nesting-rules': false
-					}
+module.exports = {
+	plugins: [
+		'postcss-import',
+		'postcss-url',
+		'postcss-nested',
+		[
+			'postcss-preset-env',
+			{
+				stage: 2,
+				features: {
+					'nesting-rules': false
 				}
-			],
-			[
-				'postcss-custom-media',
-				{
-					preserve: false
-				}
-			]
+			}
+		],
+		[
+			'postcss-custom-media',
+			{
+				preserve: false
+			}
 		]
-	}
+	]
 }
